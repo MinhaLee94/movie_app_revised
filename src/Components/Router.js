7e78912676b13abbd6bb4c9f4a78152ff6476f16
Minha Lee <minhalee94@github.com>
2021-07-21 20:46:28 -0400

    Build routers and styled component

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
-import Home from "./Routes/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import Home from "../Routes/Home"
+import TV from "../Routes/TV";
+import Search from "../Routes/Search";
+import Header from "./Header";
 
-export default () => {
+export default () => (
   <Router>
-    <Route path="/" exact component={Home}
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
-}
+);
