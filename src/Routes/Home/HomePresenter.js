fa4fbb9baf85bb5b6a7bba25fc9ceb3d82fc5c2d
MinhaLee94 <shjune1994@gmail.com>
2021-07-27 00:57:54 -0400

    Build poster component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
 import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -13,17 +14,47 @@ const HomePresenter = ({nowPlaying, popular, upcoming, loading, error}) => loadi
   <Container>
     {nowPlaying && nowPlaying.length > 0 && (
       <Section title="Now Playing">
-        {nowPlaying.map(movie => <span key={movie.id}>{movie.title}</span>)}
+        {nowPlaying.map(movie => (
+          <Poster 
+            key={movie.id} 
+            id={movie.id}
+            title={movie.original_title} 
+            imageUrl={movie.poster_path} 
+            rating={movie.vote_average}
+            isMovie={true}
+            year={movie.release_date?.substring(0, 4)}
+          />
+        ))}
       </Section>
     )}
     {upcoming && upcoming.length > 0 && (
       <Section title="Upcoming Movies">
-        {upcoming.map(movie => <span key={movie.id}>{movie.title}</span>)}
+        {upcoming.map(movie => (
+          <Poster 
+            key={movie.id} 
+            id={movie.id}
+            title={movie.original_title} 
+            imageUrl={movie.poster_path} 
+            rating={movie.vote_average}
+            isMovie={true}
+            year={movie.release_date?.substring(0, 4)}
+          />
+        ))}
       </Section>
     )}
     {popular && popular.length > 0 && (
       <Section title="Popular Movies">
-        {popular.map(movie => <span key={movie.id}>{movie.title}</span>)}
+        {popular.map(movie => (
+          <Poster 
+            key={movie.id} 
+            id={movie.id}
+            title={movie.original_title} 
+            imageUrl={movie.poster_path} 
+            rating={movie.vote_average}
+            isMovie={true}
+            year={movie.release_date?.substring(0, 4)}
+          />
+        ))}
       </Section>
     )}
     {error && <Message text={error} color="#e74c3c" />}
